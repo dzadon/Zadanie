069b02018a7f6982e6b12a0b222783f5932a2a5a
PavolDaco <palki15.daco@gmail.com>
2018-11-24 12:41:43 +0100

    Reczcler View - funkcny

--- app/src/main/java/com/example/tomdado/zadanie/MainActivity.java
+++ app/src/main/java/com/example/tomdado/zadanie/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         finish();
-        startActivity(new Intent(getApplicationContext(), UploadFileActivity.class));
+        startActivity(new Intent(getApplicationContext(), ContentActivity.class));
     }
 }
