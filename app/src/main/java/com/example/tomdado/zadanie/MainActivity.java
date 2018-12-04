9ee8544ce7e20e4fd8be76d6ccb13841a29c4596
dzadon <dzadon.tomas@gmail.com>
2018-12-04 18:53:06 +0100

    posts from DB

--- app/src/main/java/com/example/tomdado/zadanie/MainActivity.java
+++ app/src/main/java/com/example/tomdado/zadanie/MainActivity.java
@@ -25,15 +25,12 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 import android.Manifest;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -73,10 +70,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void createData() throws ExecutionException, InterruptedException {
+    private void createData() {
         final List<SingleItemModel> posts = new ArrayList<>();
         final Map<String,SingleItemModel> authors = new HashMap<>();
-       /* db.collection("users").get();.addOnSuccessListener(new OnSuccessListener<QuerySnapshot>(){
+        db.collection("users").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>(){
             @Override
             public void onSuccess(QuerySnapshot QueryDocumentSnapshots) {
                 for (QueryDocumentSnapshot document : QueryDocumentSnapshots) {
@@ -91,22 +88,8 @@ public class MainActivity extends AppCompatActivity {
                     authors.put(document.getString("email"),item);
                 }
             }
-        });*/
+        });
 
-        final QuerySnapshot querySnapshot = Tasks.await(db.collection("users").get());
-        for (DocumentSnapshot document : querySnapshot.getDocuments()) {
-            SingleItemModel item = new SingleItemModel();
-            item.setProfileView(true);
-            item.setAuthor(document.getString("email"));
-            DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-            Date date = document.getTimestamp("date").toDate();
-            String datetime =  df.format(date);
-            item.setDateTimeOfRegistration(datetime);
-            item.setNumberOfPosts(Long.toString(document.getLong("numberOfPosts")));
-            authors.put(document.getString("email"),item);
-        }
-
-/*
         db.collection("posts").orderBy("date",Query.Direction.DESCENDING).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>(){
             @Override
             public void onSuccess(QuerySnapshot QueryDocumentSnapshots) {
@@ -123,19 +106,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-*/
-        final QuerySnapshot querySnapshot2 = Tasks.await( db.collection("posts").orderBy("date",Query.Direction.DESCENDING).get());
-        for (DocumentSnapshot document : querySnapshot2.getDocuments()) {
-            SingleItemModel item = new SingleItemModel();
-            item.setProfileView(false);
-            item.setUrl(document.getString("imageurl"));
-            DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-            Date date = document.getTimestamp("date").toDate();
-            String datetime =  df.format(date);
-            item.setDateTimeOfPost(datetime);
-            item.setAuthor(document.getString("username"));
-            posts.add(item);
-        }
 
         Toast.makeText(this, "posts  '" + posts.size() , Toast.LENGTH_LONG).show();
         Toast.makeText(this, "authors  '" + authors.size() , Toast.LENGTH_LONG).show();
